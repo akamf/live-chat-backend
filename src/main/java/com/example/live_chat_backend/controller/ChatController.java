5e863b597780cfee9b4bb1710e17b11f029dd51a
Andreas Kamf <andreaskamf@gmail.com>
2025-05-30 09:18:04 +0200

    added logging to ChatController

--- src/main/java/com/example/live_chat_backend/controller/ChatController.java
+++ src/main/java/com/example/live_chat_backend/controller/ChatController.java
@@ -1,10 +1,12 @@
 package com.example.live_chat_backend.controller;
 
 import com.example.live_chat_backend.dto.ChatMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+@Slf4j
 @Controller
 public class ChatController {
 
@@ -16,6 +18,10 @@ public class ChatController {
 
     @MessageMapping("/chat")
     public void handleIncomingMessage(ChatMessage message) {
+        log.info("Received message from {}: {}", message.sender(), message.content());
         messagingTemplate.convertAndSend("/topic/chat", message);
+
+        ChatMessage fallback = new ChatMessage("System", "Echo: " + message.content(), message.timestamp());
+        messagingTemplate.convertAndSend("/topic/chat", fallback);
     }
 }
