a8da9167497051159b81e9ba093eed9689fed98a
Andreas Kamf <andreaskamf@gmail.com>
2025-06-02 22:00:19 +0200

    added ChatRoomConnection Entity

---
+++ src/main/java/com/example/live_chat_backend/entity/ChatRoomConnection.java
@@ -0,0 +1,25 @@
+package com.example.live_chat_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "chat_room_connections")
+@IdClass(ChatRoomConnection.class)
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class ChatRoomConnection {
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "chat_room_id", nullable = false)
+    private ChatRoom chatRoom;
+
+    private LocalDateTime joinedAt;
+}
