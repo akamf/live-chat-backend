c35de1e096885eda31d0385ee8922442132cd85e
Andreas Kamf <andreaskamf@gmail.com>
2025-06-02 21:54:49 +0200

    added User Entity

---
+++ src/main/java/com/example/live_chat_backend/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.live_chat_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String name;
+    private String password;
+    private LocalDateTime lastLogin;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
